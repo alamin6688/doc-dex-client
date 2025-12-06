dd26efda81463a8172a52cfbf0d7a61fad7099b1
Alamin <alamin.kzs06@gmail.com>
2025-12-07 03:07:01 +0700

    Token Handling get cookie() called in proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,13 +7,15 @@ import {
   isAuthRoute,
   UserRole,
 } from "./lib/auth-utils";
-import { deleteCookie } from "./services/auth/tokenHandlers";
+import { deleteCookie, getCookie } from "./services/auth/tokenHandlers";
 
 // This function can be marked `async` if using `await` inside
 export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  const accessToken = request.cookies.get("accessToken")?.value || null;
+  // const accessToken = request.cookies.get("accessToken")?.value || null;
+
+  const accessToken = (await getCookie("accessToken")) || null;
 
   let userRole: UserRole | null = null;
   if (accessToken) {
