6824e5a4833335ead7967cac39a5d4e36468fe57
Alamin <alamin.kzs06@gmail.com>
2026-01-17 04:18:09 +0600

    Completed Payment And Caching Issue Of Appointments

--- src/services/auth/getUserInfo.ts
+++ src/services/auth/getUserInfo.ts
@@ -27,7 +27,7 @@ export const getUserInfo = async (): Promise<UserInfo | any> => {
 
       const verifiedToken = jwt.verify(
         accessToken,
-        process.env.JWT_SECRET as string,
+        process.env.ACCESS_TOKEN_SECRET as string,
       ) as JwtPayload;
 
       userInfo = {
